0f144e9a199086f61fa2de4b0c7e5ed805ec5783
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-03-11 15:58:44 -0600

    Creating the basics of User model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model} = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true,
+    },
+    email: {
+        type: String, 
+        required: 'Email address is required',
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: {
+        //Array of "_id" values referencing the Thought model
+
+    },
+    friends: {
+        //Array of "_id" values referencing the User model (self-reference)
+
+    }
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
