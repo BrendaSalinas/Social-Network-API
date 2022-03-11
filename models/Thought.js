c6836941c37cd50596f32bd5e40a381feb6af60e
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-03-11 16:15:01 -0600

    Setting up the begining of the Thought model

---
+++ models/Thought.js
@@ -0,0 +1,20 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String, 
+        required: true, 
+        //must be between 1 to 280 characters
+        arguments: [1,280]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, 
+    },
+    username: {
+    //the user that created this thought
+        type: String, 
+        required: true,
+    },
+    reactions: [ReactionSchema]
+});
