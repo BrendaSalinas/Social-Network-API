e4b6e6f6160403fd8c19cadf4452b23782d1e7ed
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-03-13 09:25:47 -0500

    adding the POST route

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -23,5 +23,24 @@ const thoughtController = {
             res.status(500).json(err)
         });
     },
-    
+    //Post a new thought
+    createThought({params, body}, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { username: body.username},
+                { $push: { thoughts: _id }},
+                { new: true }
+            )
+        })
+        .then(dbUserData => {
+            if(!dbUserData){ 
+                res.status(404).json({ message: 'No user found with this id! '})
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => res.json(err));
+    },
+    //
 }
