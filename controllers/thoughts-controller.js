c9584834d6583d7b18b9ae2f5f1106fc32ce96bd
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-03-13 09:14:32 -0500

    Creating the first get routes for thought controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,2 +1,27 @@
 const { User, Thought } = require('../models');
 
+const thoughtController = {
+    //get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughData => res.json(dbThoughData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    //get a single thought by its _id
+    getThoughtById({ params, }, res) {
+        Thought.findOne({ _id: params.id })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughData => res.json(dbThoughData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    },
+    
+}
